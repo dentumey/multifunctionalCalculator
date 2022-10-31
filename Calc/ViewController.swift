6e22ffbad16817829095b628a7a5f05c0f42a60b
Den <dentumey@gmail.com>
2022-10-31 21:46:37 +0300

    Initial Commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Dentumei Lutfullin on 31.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
